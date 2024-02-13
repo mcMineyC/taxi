84e13903ad8c557194dc0240770dc02701753e68
mcMineyC <jedi.master@falconhosting.net>
2024-02-12 17:53:21 -0800

    Fix a ton of stuff. Sidebar: change icons and button type Media controls: add z-index of 9999999999999999999999 Main page?: give up horizontal scrolling, use flex wrapping instead.

--- server/server.js
+++ server/server.js
@@ -11,8 +11,131 @@ app.use('/music', express.static(path.join(__dirname, 'music')));
 
 app.use('/',express.static(path.join(__dirname, 'static')));
 
+app.get('/info/all', function (req, res) {
+    res.sendFile(path.join(__dirname, 'all.json'));
+});
+
+app.get('/info/albums', function (req, res) {
+    var data = fs.readFileSync(path.join(__dirname, 'albums.json'), 'utf-8');
+    var all = fs.readFileSync(path.join(__dirname, 'all.json'), 'utf-8');
+    data = JSON.parse(data);
+    all = JSON.parse(all);
+    var albums_data = {
+        "last_updated": all,
+    }
+    var albums_arr = [];
+
+    //This is only needed if the file changed
+    if((JSON.stringify(data["last_updated"]) != JSON.stringify(all))){
+        console.log("Updating")
+        for(var x = 0; x < (all["entries"].length); x++){
+            artist = all["entries"][x]
+            console.log(artist["displayName"]); //artist
+            for (var album in artist["albums"]) {
+                var albumid = album;
+                var album = artist["albums"][album];
+                console.log("\t" + album["displayName"]); //album
+                var warr = {
+                    "id": albumid,
+                    "displayName": album["displayName"],
+                    "artist": artist["displayName"],
+                    "artistId": artist["id"]
+                }
+                albums_arr.push(warr)
+            }
+        }
+        albums_data["albums"] = albums_arr
+        console.log("\n\n")
+        console.log(albums_data)
+        fs.writeFile(path.join(__dirname, 'albums.json'), JSON.stringify(albums_data), function (err) {
+            if (err) {
+                console.log(err);
+            }
+        })
+        data = albums_data
+    }
+    res.send(data);
+});
+
+app.get('/info/artists', function (req, res) {
+    var data = fs.readFileSync(path.join(__dirname, 'artists.json'), 'utf-8');
+    var all = fs.readFileSync(path.join(__dirname, 'all.json'), 'utf-8');
+    data = JSON.parse(data);
+    all = JSON.parse(all);
+    var artist_data = {
+        "last_updated": all,
+    }
+    var artist_arr = [];
+
+    //This is only needed if the file changed
+    if((JSON.stringify(data["last_updated"]) != JSON.stringify(all))){
+        console.log("Updating")
+        for(var x = 0; x < (all["entries"].length); x++){
+            artist = all["entries"][x]
+            console.log(artist["displayName"]);
+            var warr = {
+                "id": artist["id"],
+                "displayName": artist["displayName"],
+            }
+            artist_arr.push(warr)
+        }
+        artist_data["artists"] = artist_arr
+        console.log("\n\n")
+        console.log(artist_data)
+        fs.writeFile(path.join(__dirname, 'artists.json'), JSON.stringify(artist_data), function (err) {
+            if (err) {
+                console.log(err);
+            }
+        })
+        data = artist_data
+    }
+    res.send(data);
+});
+
 app.get('/info/songs', function (req, res) {
-    res.sendFile(path.join(__dirname, 'songs.json'));
+    var data = fs.readFileSync(path.join(__dirname, 'songs.json'), 'utf-8');
+    var all = fs.readFileSync(path.join(__dirname, 'all.json'), 'utf-8');
+    data = JSON.parse(data);
+    all = JSON.parse(all);
+    var albums_data = {
+        "last_updated": all,
+    }
+    var albums_arr = [];
+
+    //This is only needed if the file changed
+    if((JSON.stringify(data["last_updated"]) != JSON.stringify(all))){
+        console.log("Updating")
+        for(var x = 0; x < (all["entries"].length); x++){
+            artist = all["entries"][x]
+            console.log(artist["displayName"]); //artist
+            for (var album in artist["albums"]) {
+                var albumid = album;
+                var album = artist["albums"][album];
+                console.log("\t" + album["displayName"]); //album
+                Object.entries(album["songs"]).forEach((song) => {
+                    var [key, v] = song;
+                    console.log("\t\t" + v["title"] + ": " + v["file"] + ""); //song
+                
+                    var warr = {
+                        "id": v["id"],
+                        "displayName": v["title"],
+                        "file": v["file"]
+                    }
+                    albums_arr.push(warr)
+                });
+            }
+        }
+        albums_data["songs"] = albums_arr
+        console.log("\n\n")
+        console.log(albums_data)
+        fs.writeFile(path.join(__dirname, 'songs.json'), JSON.stringify(albums_data), function (err) {
+            if (err) {
+                console.log(err);
+            }
+        })
+        data = albums_data
+    }
+    res.send(data);
 });
 
 app.listen(port, () => {
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,14 +4,14 @@ class sidebar extends HTMLElement {
         <div id="sidebar" class="sidebar">
             <div class="sidebar-align-center">
                 <md-fab>
-                    <md-icon slot="icon">menu</md-icon>
+                    <md-icon slot="icon">search</md-icon>
                 </md-fab>
-                <md-fab lowered size="small">
-                    <md-icon slot="icon">menu</md-icon>
-                </md-fab>
-                <md-fab lowered size="small">
-                    <md-icon slot="icon">menu</md-icon>
-                </md-fab>
+                <md-filled-icon-button>
+                    <md-icon>menu</md-icon>
+                </md-filled-icon-button>
+                <md-icon-button>
+                    <md-icon>menu</md-icon>
+                </md-icon-button>
             </div>
         </div>  
       `;
