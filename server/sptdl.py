45fd41faafef328726e08106f56d2a88bd74c399
mcMineyC <jedi.master@falconhosting.net>
2024-03-24 21:25:55 -0700

    Minor update

--- server/sptdl.py
+++ server/sptdl.py
@@ -12,9 +12,9 @@ def printer(junk, message):
     print(json.dumps({"message": message, "completed": completed, "total": total, "name": name}))
 
 obj = Spotdl(client_id="0a65ebdec6ec4983870a7d2f51af2aa1", client_secret="22714014e04f46cebad7e03764beeac8", no_cache=True)
-
 print(json.dumps({"message": "Starting download", "completed": 0, "total": 0, "name": ""}))
 songs_obj = obj.search(sys.argv[1:])
+print(json.dumps({"message": "Starting download", "completed": 0, "total": len(songs_obj), "name": ""}))
 obj.downloader.progress_handler.close()
 dl = Downloader(DownloaderOptions(simple_tui=True))
 obj.downloader.progress_handler.close()
